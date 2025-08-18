a1973f9f3471a8d7989931e6ff63bbda986e2769
IsZeera <122827732+IsZeera@users.noreply.github.com>
2025-08-18 00:42:45 -0300

    Actualizar asu.js

--- asu.js
+++ asu.js
@@ -1,2 +1,7 @@
-console.log("fernando");
+const boton_menu = document.querySelector('.button_btn');
+const links = document.querySelector('.link_2_nav');
 
+
+boton_menu.addEventListener("click", () => {
+    links.classList.toggle("active");
+})
